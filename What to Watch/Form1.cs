0e7d2902eadbf61fccf9db23dbadd18fa8b63c4d
TennantQuinton <quintonimo1998@gmail.com>
2021-11-26 14:30:17 -0500

    Update main cs

--- What to Watch/Form1.cs
+++ What to Watch/Form1.cs
@@ -12,6 +12,8 @@ namespace What_to_Watch
 {
     public partial class Form1 : Form
     {
+        // TODO: Add documentary folder and make sure it's working here
+        // TODO: Add way to open all other programs here and there
         public Form1()
         {
             InitializeComponent();
